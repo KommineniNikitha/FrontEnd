7d3ff92325d3d660f9e58a63ea3ce1a4bc58acc0
KommineniNikitha <kommineninikitha@gmail.com>
2018-07-01 16:44:36 +0530

    saved coding challenge

--- Javascript/codingchallenge-7.js
+++ Javascript/codingchallenge-7.js
@@ -7,23 +7,20 @@ var ques1 = new Question('1.what is your favourite name?', ['nikki', 'puppy', 'a
 var ques2 = new Question('2. what is your profession?', ['programmer', 'JS learner', 'home maker'], '1');
 var ques3 = new Question('3. where are you right now?', ['banglore', 'khammam', 'tammara'], '0');
 
-var storearray=[ques1,ques2,ques3];
-var selectques=Math.floor(Math.random()*3);
+var storearray = [ques1, ques2, ques3];
+var selectques = Math.floor(Math.random() * 3);
 
-Question.prototype.displayQuestion=function()
-{
+Question.prototype.displayQuestion = function() {
   console.log(this.question);
-  for(var i=0;i<storearray.length;i++)
-  {
-  console.log(i+ ':' +this.choices[i]);
+  for (var i = 0; i < storearray.length; i++) {
+    console.log(i + ':' + this.choices[i]);
   }
-var n = prompt("Please enter your name");
-if(n=== this.answer) {
-console.log("correct answer");
-}
-else{
-console.log("wrong answer try again:");
-}
+  var n = prompt("Please enter your name");
+  if (n === this.answer) {
+    console.log("correct answer");
+  } else {
+    console.log("wrong answer try again:");
+  }
 }
 storearray[selectques].displayQuestion();
 
